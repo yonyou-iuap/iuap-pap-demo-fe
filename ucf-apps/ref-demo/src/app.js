63416c5d54098ca0d59f471ac12f9a3b0023db8f
yaoxinc <yaoxinc@yonyou.com>
2019-03-27 17:15:36 +0800

    修改多语demo

--- ucf-apps/ref-demo/src/app.js
+++ ucf-apps/ref-demo/src/app.js
@@ -1,20 +1,153 @@
 /**
  * 入口、导入组件样式、渲染
  */
-import React from "react";
-import {render, Router} from "mirrorx";
-import {getCookie} from './util';
 
+import React,{Component} from 'react';
+import { render } from 'mirrorx';
+import 'ucf-common/styles/tinper-bee.css';
+import 'ucf-common/styles/public.less';
+import './app.less';
+import {getCookie} from './util';
 let lang = getCookie('u_locale') || getCookie('i18next') || 'zh_CN';
-import AppCN from './app_cn';
-import AppTW from './app_tw';
-import AppEN from './app_en';
-let App;
-if(lang === 'zh_CN'){
-  App = AppCN;
-} else if(lang === 'zh_TW'){
-  App = AppTW;
-}else if(lang === 'en_US'){
-  App = AppEN;
-}
-render(<App />, document.querySelector("#app"));
+let dir = lang.toLocaleLowerCase()==='en_us'?'Ref_EN':(lang.toLocaleLowerCase()==='zh_tw'?'Ref_TW':'Ref');
+let {  
+  RefTreeDemo1,
+  RefTreeDemo2,
+  RefTreeDemo3,
+  RefTreeDemo4,
+  RefTreeDemo5,
+  RefTreeDemo6,} =  require(`./components/${dir}/RefTree`)
+
+  let {
+    RefTableDemo1,
+    RefTableDemo2,
+    RefTableDemo3,
+    RefTableDemo4,
+  } =  require(`./components/${dir}/RefTable`)
+  
+  let {
+    RefTreeTransferDemo1,
+    RefTreeTransferDemo2
+  } =  require(`./components/${dir}/RefTreeTransfer`)
+  let {
+    RefTreeTableDemo1,
+  } =  require(`./components/${dir}/RefTreeTable`)
+  
+  let {
+    RefJiLianDemo1,
+    RefJiLianDemo2,
+    RefJiLianDemo3,
+  } =  require(`./components/${dir}/RefJiLian`)
+
+  
+  let {RefComboboxDemo1,RefComboboxDemo2} =  require(`./components/${dir}/RefCombobox`)
+  class App extends Component {
+    constructor() {
+      super();
+      this.state = {
+        checkedArray: [],
+        value: '',
+        count: [],
+        refpk: '',
+        refname: '',
+        strictMode: false
+      }
+  
+      setTimeout(() => {
+        this.setState({
+          strictMode: true
+        });
+      })
+    }
+  
+  
+    render() {
+      let { strictMode } = this.state;
+      return (
+        <div>
+          <h2>树参照</h2>
+          <div className="demo-row">
+            <div className="demo-col">
+              <RefTreeDemo1 lang={lang}/>
+            </div>
+            <div className="demo-col">
+              <RefTreeDemo2 lang={lang}/>
+            </div>
+            <div className="demo-col">
+              <RefTreeDemo3 lang={lang}/>
+            </div>
+            <div className="demo-col">
+              <RefTreeDemo4 lang={lang}/>
+            </div>
+            <div className="demo-col">
+              <RefTreeDemo5 lang={lang}/>
+            </div>
+            <div className="demo-col">
+              <RefTreeDemo6 lang={lang}/>
+            </div>
+          </div>
+          <h2>表格参照</h2>
+          <div className="demo-row">
+            <div className="demo-col">
+              <RefTableDemo1 lang={lang}/>
+            </div>
+            <div className="demo-col">
+              <RefTableDemo2 lang={lang}/>
+            </div>
+          </div>
+          <div className="demo-row">
+            <div className="demo-col">
+              <RefTableDemo3 lang={lang}
+              />
+            </div>
+          </div>
+          <div className="demo-row">
+            <div className="demo-col">
+              <RefTableDemo4 lang={lang}
+              />
+            </div>
+          </div>
+          <h2>级联参照</h2>
+          <div className="demo-row">
+            <div className="demo-col">
+              <RefJiLianDemo1 lang={lang}/>
+            </div>
+            <div className="demo-col">
+              <RefJiLianDemo2 lang={lang}/>
+            </div>
+            <div className="demo-col">
+              <RefJiLianDemo3 lang={lang}/>
+            </div>
+          </div>
+          
+          <h2>穿梭参照</h2>
+          <div className="demo-row">
+            <div className="demo-col">
+              <RefTreeTransferDemo1 lang={lang}/>
+            </div>
+            <div className="demo-col">
+              <RefTreeTransferDemo2 lang={lang}/>
+            </div>
+          </div>
+          <h2>树表参照</h2>
+          <div className="demo-row">
+            <div className="demo-col">
+              <RefTreeTableDemo1 lang={lang}
+              />
+            </div>
+          </div>
+          <h2>combobox参照</h2>
+          <div className="demo-row">
+            <div className="demo-col">
+              <RefComboboxDemo1 lang={lang}/>
+            </div>
+            <div className="demo-col">
+              <RefComboboxDemo2 lang={lang}/>
+            </div>
+          </div>
+        </div>
+      )
+    }
+  };
+  
+render(<App/>, document.querySelector("#app"));
