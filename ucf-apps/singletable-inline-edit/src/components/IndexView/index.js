f0c0324a09d5459d46ce866594b30d4daa30c51e
“jonyshi” <jonyshi@126.com>
2019-05-23 08:24:51 +0800

    grid升级,删除等问题

--- ucf-apps/singletable-inline-edit/src/components/IndexView/index.js
+++ ucf-apps/singletable-inline-edit/src/components/IndexView/index.js
@@ -540,7 +540,6 @@ class IndexView extends Component {
             key: uuid(),
             ...this.newDataTmp
         }
-        // debugger;
         //当第一次新增的时候
         // 禁用其他checked
         //重置表头状态
